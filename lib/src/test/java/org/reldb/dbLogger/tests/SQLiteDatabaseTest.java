08cf07d39fc681bcf68f868d921b8dd1b06d0d0b
dave <dave@armchair.mb.ca>
2020-10-20 23:24:33 +0100

    Provide test for logging nested maps.

--- lib/src/test/java/org/reldb/dbLogger/tests/SQLiteDatabaseTest.java
+++ lib/src/test/java/org/reldb/dbLogger/tests/SQLiteDatabaseTest.java
@@ -13,19 +13,44 @@ class SQLiteDatabaseTest {
 
     @Test void testLogBasic() {
         try (var db = new SQLiteDatabase("./testdb.sqlite")) {
-            var log = new Log(db.getConnection(), "testlog");
-            for (int i = 0; i < 10; i++) {
-                Logger.log("a", i)
-                        .log("b", "" + i)
-                        .log("c", "blah")
-                        .insert(log);
+            try (var log = new Log(db.getConnection(), "testlog")) {
+                for (int i = 0; i < 10; i++) {
+                    Logger.log("a", i)
+                            .log("b", "" + i)
+                            .log("c", "blah")
+                            .insert(log);
+                }
+                for (int i = 11; i < 20; i++) {
+                    Logger.log("a", i)
+                            .log("b", "" + i)
+                            .log("c", "blah")
+                            .log("d", (float) i)
+                            .insert(log);
+                }
             }
-            for (int i = 11; i < 20; i++) {
-                Logger.log("a", i)
-                        .log("b", "" + i)
-                        .log("c", "blah")
-                        .log("d", (float) i)
-                        .insert(log);
+        }
+    }
+
+    @Test void testLogNested1() {
+        try (var db = new SQLiteDatabase("./testdb.sqlite")) {
+            try (var log = new Log(db.getConnection(), "testlognested")) {
+                for (int i = 0; i < 10; i++) {
+                    Logger.log("a", i)
+                            .log("b", "" + i)
+                            .log("c", "blah")
+                            .log("n",
+                                    Logger.log("p", i * 10)
+                                            .log("q", (double) (i * 20))
+                                            .log("a", i + 100))
+                            .insert(log);
+                }
+                for (int i = 11; i < 20; i++) {
+                    Logger.log("a", i)
+                            .log("b", "" + i)
+                            .log("c", "blah")
+                            .log("d", (float) i)
+                            .insert(log);
+                }
             }
         }
     }
